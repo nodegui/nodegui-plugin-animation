28d4491e6fbaadc2cce5f32fdc5c8bf549bafc60
Atul R <atulanand94@gmail.com>
2019-11-09 14:46:46 +0100

    abstractanimation all methods except group

--- src/cpp/QAbstractAnimation/qabstractanimation_macro.h
+++ src/cpp/QAbstractAnimation/qabstractanimation_macro.h
@@ -12,29 +12,136 @@
  */
 
 #ifndef QABSTRACTANIMATION_WRAPPED_METHODS_DECLARATION
-#define QABSTRACTANIMATION_WRAPPED_METHODS_DECLARATION \
-  QOBJECT_WRAPPED_METHODS_DECLARATION                  \
-                                                       \
-  Napi::Value start(const Napi::CallbackInfo& info) {  \
-    Napi::Env env = info.Env();                        \
-    Napi::HandleScope scope(env);                      \
-    this->instance->start();                           \
-    return env.Null();                                 \
-  }                                                    \
-                                                       \
-  Napi::Value stop(const Napi::CallbackInfo& info) {   \
-    Napi::Env env = info.Env();                        \
-    Napi::HandleScope scope(env);                      \
-    this->instance->stop();                            \
-    return env.Null();                                 \
-  }                                                    \
+#define QABSTRACTANIMATION_WRAPPED_METHODS_DECLARATION                         \
+  QOBJECT_WRAPPED_METHODS_DECLARATION                                          \
+                                                                               \
+  Napi::Value start(const Napi::CallbackInfo& info) {                          \
+    Napi::Env env = info.Env();                                                \
+    Napi::HandleScope scope(env);                                              \
+    if (info.Length == 1) {                                                    \
+      Napi::Number deletionPolicy = info[0].As<Napi::Number>();                \
+      this->instance->start(static_cast<QAbstractAnimation::DeletionPolicy>(   \
+          deletionPolicy.Int32Value()));                                       \
+    } else {                                                                   \
+      this->instance->start();                                                 \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value stop(const Napi::CallbackInfo& info) {                         \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      this->instance->stop();                                                  \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value currentLoop(const Napi::CallbackInfo& info) {                  \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int loop = this->instance->currentLoop();                                \
+      return Napi::Value::From(env, loop);                                     \
+    }                                                                          \
+    Napi::Value currentLoopTime(const Napi::CallbackInfo& info) {              \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int time = this->instance->currentLoopTime();                            \
+      return Napi::Value::From(env, time);                                     \
+    }                                                                          \
+    Napi::Value currentTime(const Napi::CallbackInfo& info) {                  \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int time = this->instance->currentTime();                                \
+      return Napi::Value::From(env, time);                                     \
+    }                                                                          \
+    Napi::Value direction(const Napi::CallbackInfo& info) {                    \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int value = static_cast<int>(this->instance->direction());               \
+      return Napi::Value::From(env, value);                                    \
+    }                                                                          \
+    Napi::Value duration(const Napi::CallbackInfo& info) {                     \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int duration = this->instance->duration();                               \
+      return Napi::Value::From(env, duration);                                 \
+    }                                                                          \
+    Napi::Value loopCount(const Napi::CallbackInfo& info) {                    \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int loopCount = this->instance->loopCount();                             \
+      return Napi::Value::From(env, loopCount);                                \
+    }                                                                          \
+    Napi::Value setDirection(const Napi::CallbackInfo& info) {                 \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      Napi::Number directionValue = info[0].As<Napi::Number>();                \
+      this->instance->setDirection(                                            \
+          static_cast<QAbstractAnimation::Direction>(direction.Int32Value())); \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value setLoopCount(const Napi::CallbackInfo& info) {                 \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      Napi::Number loopCount = info[0].As<Napi::Number>();                     \
+      this->instance->setLoopCount(loopCount.Int32Value());                    \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value state(const Napi::CallbackInfo& info) {                        \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int value = static_cast<int>(this->instance->state());                   \
+      return Napi::Value::From(env, value);                                    \
+    }                                                                          \
+    Napi::Value totalDuration(const Napi::CallbackInfo& info) {                \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      int value = this->instance->totalDuration();                             \
+      return Napi::Value::From(env, value);                                    \
+    }                                                                          \
+    Napi::Value pause(const Napi::CallbackInfo& info) {                        \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      this->instance->pause();                                                 \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value resume(const Napi::CallbackInfo& info) {                       \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      this->instance->resume();                                                \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value setCurrentTime(const Napi::CallbackInfo& info) {               \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      Napi::Number time = info[0].As<Napi::Number>();                          \
+      this->instance->setCurrentTime(time.Int32Value());                       \
+      return env.Null();                                                       \
+    }                                                                          \
+    Napi::Value setPaused(const Napi::CallbackInfo& info) {                    \
+      Napi::Env env = info.Env();                                              \
+      Napi::HandleScope scope(env);                                            \
+      Napi::Boolean value = info[0].As<Napi::Boolean>();                       \
+      this->instance->setPaused(value.Value());                                \
+      return env.Null();                                                       \
+    }                                                                          \
 #endif
 
 #ifndef QABSTRACTANIMATION_WRAPPED_METHODS_EXPORT_DEFINE
-#define QABSTRACTANIMATION_WRAPPED_METHODS_EXPORT_DEFINE(ComponentWrapName) \
-  QOBJECT_WRAPPED_METHODS_EXPORT_DEFINE(ComponentWrapName),                 \
-                                                                            \
-      InstanceMethod("setViewport", &ComponentWrapName::setViewport),       \
-      InstanceMethod("viewport", &ComponentWrapName::viewport),
+#define QABSTRACTANIMATION_WRAPPED_METHODS_EXPORT_DEFINE(ComponentWrapName)   \
+  QOBJECT_WRAPPED_METHODS_EXPORT_DEFINE(ComponentWrapName),                   \
+                                                                              \
+      InstanceMethod("start", &ComponentWrapName::start),                     \
+      InstanceMethod("stop", &ComponentWrapName::stop),                       \
+      InstanceMethod("currentLoop", &ComponentWrapName::currentLoop),         \
+      InstanceMethod("currentLoopTime", &ComponentWrapName::currentLoopTime), \
+      InstanceMethod("currentTime", &ComponentWrapName::currentTime),         \
+      InstanceMethod("direction", &ComponentWrapName::direction),             \
+      InstanceMethod("duration", &ComponentWrapName::duration),               \
+      InstanceMethod("loopCount", &ComponentWrapName::loopCount),             \
+      InstanceMethod("setDirection", &ComponentWrapName::setDirection),       \
+      InstanceMethod("setLoopCount", &ComponentWrapName::setLoopCount),       \
+      InstanceMethod("state", &ComponentWrapName::state),                     \
+      InstanceMethod("totalDuration", &ComponentWrapName::totalDuration),     \
+      InstanceMethod("pause", &ComponentWrapName::pause),                     \
+      InstanceMethod("resume", &ComponentWrapName::resume),                   \
+      InstanceMethod("setCurrentTime", &ComponentWrapName::setCurrentTime),   \
+      InstanceMethod("setPaused", &ComponentWrapName::setPaused),
 
 #endif
--- src/lib/QAbstractAnimation.ts
+++ src/lib/QAbstractAnimation.ts
@@ -1,10 +1,54 @@
 import { QObject } from '@nodegui/nodegui';
 
 export abstract class QAbstractAnimation extends QObject {
-    start(): void {
-        this.native.start();
+    start(policy: DeletionPolicy): void {
+        this.native.start(policy);
     }
     stop(): void {
         this.native.stop();
     }
+    currentLoop(): number {
+        return this.native.currentLoop();
+    }
+    currentLoopTime(): number {
+        return this.native.currentLoopTime();
+    }
+    currentTime(): number {
+        return this.native.currentTime();
+    }
+    direction(): Direction {
+        return this.native.direction();
+    }
+    duration(): number {
+        return this.native.duration();
+    }
+    loopCount(): number {
+        return this.native.loopCount();
+    }
+    setDirection(direction: Direction): void {
+        return this.native.setDirection(direction);
+    }
+    setLoopCount(loopCount: number): void {
+        return this.native.setLoopCount(loopCount);
+    }
+    state(): State {
+        return this.native.state();
+    }
+    totalDuration(): number {
+        return this.native.totalDuration();
+    }
+}
+
+export enum DeletionPolicy {
+    KeepWhenStopped = 0,
+    DeleteWhenStopped = 1,
+}
+export enum Direction {
+    Forward = 0,
+    Backward = 1,
+}
+export enum State {
+    Stopped = 0,
+    Paused = 1,
+    Running = 2,
 }
